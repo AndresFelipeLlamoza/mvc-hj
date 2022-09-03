799757759484b6cc30ff07867c24b52dd819a758
Andres Felipe Llamoza <andres.llamoza@misena.edu.co>
2022-09-03 10:09:15 -0500

    Reporte terminado por andres felipe llamosa

--- model/reporte.php
+++ model/reporte.php
@@ -7,15 +7,19 @@ class PDF extends FPDF
 function Header()
 {
     // Logo
-    $this->Image('../img/Logo.png',160,2,45,0,'PNG');    
+    $this->Image('../img/Logo.png',10,2,45,0,'PNG');    
     // Arial bold 15
-    $this->SetFont('Arial','B',15);
+    $this->SetFont('Arial','B',14);
     // Movernos a la derecha
     $this->Cell(60);
     // Título
-    $this->Cell(70,10,'Reporte de usuarios',1,0,'C');
+    $this->Cell(70,10,'Reporte de usuarios',0,0,'C');
     // Salto de línea
-    $this->Ln(20);
+    $this->Ln(30);
+    $this->Cell(65, 10, 'Nombre', 1, 0, 'C', 0);
+    $this->Cell(75, 10, 'Correo', 1, 0, 'C', 0);
+    $this->Cell(50, 10, 'idRol', 1, 1, 'C', 0);
+    
 }
 
 // Pie de página
@@ -29,10 +33,20 @@ function Footer()
     $this->Cell(0,10,utf8_decode('Página ').$this->PageNo().'/{nb}',0,0,'C');
 }
 }
+include ('conexion.php');
+$consulta = "SELECT * FROM usuarios";
+$resultado = mysqli_query($conx, $consulta);
+
 $pdf = new PDF();
 $pdf-> AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10, utf8_decode('¡Hola!, esta es una prueba de la libreria de FPDF'));
+$pdf->SetFont('Arial','',16);
+
+foreach ($conx -> query($consulta) as $row){
+    
+    $pdf->Cell(65, 10, $row['Nombre'], 1, 0, 'C', 0);
+    $pdf->Cell(75, 10, $row['Correo'], 1, 0, 'C', 0);
+    $pdf->Cell(50, 10, $row['idRol'], 1, 1, 'C', 0);
+}
 $pdf->Output();
 ?>
